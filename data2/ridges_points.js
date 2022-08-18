d5cac0c4c54d6ffc8a2998c11f744187bb3ccfef
Julia <>
2022-08-18 11:05:37 +0200

    first commit

---
+++ data2/ridges_points.js
@@ -0,0 +1,38 @@
+const ridges_points = [
+	{
+	   "Name":"北冰洋洋中脊",
+	   "Type":"point",
+	   "Location references for Julia":"https://marineregions.org/gazetteer.php?p=details&id=7819",
+	   "latitude":86.55921619,
+	   "longitude":77.85966444,
+	   "Period_of_formation":"110万年前",
+	   "Height":"N/A",
+	   "Depth":"N/A",
+	   "Length":"1800公里",
+	   "Description":"这条又名加科尔海脊的火山脊，形成了北美板块与欧亚板块之间的边界。苏联探险队于1948年首次发现了它。其最大的火山曾于大约110万年前喷发。"
+	},
+	{
+	   "Name":"探险家海脊",
+	   "Type":"point",
+	   "Location references for Julia":"http://marineregions.org/mrgid/19275",
+	   "latitude":49.45,
+	   "longitude":-130.58333,
+	   "Period_of_formation":"500万到700万年前",
+	   "Height":"N/A",
+	   "Depth":"N/A",
+	   "Length":"N/A",
+	   "Description":"这条海脊位于加拿大英属哥伦比亚温哥华岛以西241公里处。它的地质活动活跃，导致这一区域时常发生地震。"
+	},
+	{
+	   "Name":"戈尔达海脊",
+	   "Type":"point",
+	   "Location references for Julia":"http://marineregions.org/mrgid/6404",
+	   "latitude":41.75,
+	   "longitude":-127,
+	   "Period_of_formation":"3000万年前",
+	   "Height":"N/A",
+	   "Depth":"N/A",
+	   "Length":"300公里",
+	   "Description":"它的起源来自3000万年前法拉隆板块俯冲到北美板块下方，这个过程形成了新的小板块和海脊，包括胡安·德·富卡洋板块。而这些小板块和海脊的俯冲又形成了戈尔达海脊和戈尔达板块。"
+	}
+ ]
---
+++ data2/seamounts_points.js
@@ -0,0 +1,38 @@
+const seamounts_points = [
+	{
+	   "Name":"冒纳凯阿火山",
+	   "Coordinate_type":"point",
+	   "Location_references":"",
+	   "latitude":19.820667,
+	   "longitude":-155.468056,
+	   "Period_of_formation":"至少100万年前",
+	   "Height":"9966米（4205米在海平面以上）",
+	   "Length":"N/A",
+	   "Description":"它是组成夏威夷群岛的六座火山之一。如果从水下的山脚开始测算，这座火山的高度比珠穆朗玛峰还高1116米。这座火山目前处于休眠状态，上一次喷发还是在4600年前。",
+	   "Biodiversity":"这一地区可见各类海洋动物，从座头鲸（humpback whale）和夏威夷粉鲷（Hawaiian pink snapper）到海参和深海鳕鳗（Abyssal cusk eel）。"
+	},
+	{
+	   "Name":"阶梯海山",
+	   "Coordinate_type":"point",
+	   "Location_references":"http://marineregions.org/mrgid/5856",
+	   "latitude":-43.90776276,
+	   "longitude":150.4629842,
+	   "Period_of_formation":"3390万年到5600万年前",
+	   "Height":"最高点到达海平面以下598米",
+	   "Length":"N/A",
+	   "Description":"这座海底山曾是一座海拔400米的火山。它目前位于东塔斯曼山海高原上，1944年被发现。",
+	   "Biodiversity":"若干种类的鲸鱼、信天翁和海燕生活在这一带。深海海星 <i>Novodinia australis </i>也居住在这座海山所在的区域。"
+	},
+	{
+	   "Name":"轴向海山",
+	   "Coordinate_type":"point",
+	   "Location_references":"http://marineregions.org/mrgid/18840",
+	   "latitude":45.94501,
+	   "longitude":-129.93724,
+	   "Period_of_formation":"3.1万年前",
+	   "Height":"比胡安·德·富卡洋中脊（Juan de Fuca Ridge）平均水平高出700米",
+	   "Length":"N/A",
+	   "Description":"这座位于胡安·德·富卡洋中脊的深海活火山，曾在2015年喷发，是东北太平洋海盆最活跃的火山。",
+	   "Biodiversity":"附近海域生活在多个品种的硬骨鱼。蜘蛛蟹和铠甲虾在这里也很常见。此外，还有海星、蛇尾、海胆和海参。"
+	}
+ ]
